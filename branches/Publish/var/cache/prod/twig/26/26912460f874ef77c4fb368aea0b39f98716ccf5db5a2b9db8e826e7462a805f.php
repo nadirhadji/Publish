223f3bd0f01fc810bed52a0a633e40837dd5e701
ih06448 <ih06448@269467bf-4d7a-42d7-80bb-480cca416ccd>
2019-03-09 18:53:42 +0000

    Ajout derniere version Publish dans Branche

---
+++ branches/Publish/var/cache/prod/twig/26/26912460f874ef77c4fb368aea0b39f98716ccf5db5a2b9db8e826e7462a805f.php
@@ -0,0 +1,57 @@
+<?php
+
+/* @FOSUser/Group/new.html.twig */
+class __TwigTemplate_345fe805e28be5a432134906ad1b76e7d73a4290405ac4d8abd836a5005e39c8 extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        // line 1
+        $this->parent = $this->loadTemplate("@FOSUser/layout.html.twig", "@FOSUser/Group/new.html.twig", 1);
+        $this->blocks = [
+            'fos_user_content' => [$this, 'block_fos_user_content'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "@FOSUser/layout.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_fos_user_content($context, array $blocks = [])
+    {
+        // line 4
+        $this->loadTemplate("@FOSUser/Group/new_content.html.twig", "@FOSUser/Group/new.html.twig", 4)->display($context);
+    }
+
+    public function getTemplateName()
+    {
+        return "@FOSUser/Group/new.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  35 => 4,  32 => 3,  15 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "@FOSUser/Group/new.html.twig", "/Applications/MAMP/htdocs/Publish/vendor/friendsofsymfony/user-bundle/Resources/views/Group/new.html.twig");
+    }
+}
---
+++ branches/Publish/var/cache/prod/twig/ac/accdb5de8370259ebac35a26abc00065bfe29fbda6692fb0a36388bd45e8cc83.php
@@ -0,0 +1,41 @@
+<?php
+
+/* @Framework/Form/repeated_row.html.php */
+class __TwigTemplate_7161f3b83521713975835b24b0552a43d1d1fe4fbee0546b9285799301332822 extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<?php echo \$view['form']->block(\$form, 'form_rows') ?>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/Form/repeated_row.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "@Framework/Form/repeated_row.html.php", "/Applications/MAMP/htdocs/Publish/vendor/symfony/symfony/src/Symfony/Bundle/FrameworkBundle/Resources/views/Form/repeated_row.html.php");
+    }
+}
