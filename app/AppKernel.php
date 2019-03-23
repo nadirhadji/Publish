225716b496e5139d03a192b934137357b128e7b9
ii11124 <ii11124@269467bf-4d7a-42d7-80bb-480cca416ccd>
2019-03-23 04:12:30 +0000

    Override validation.yml par default issue de FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ConnexionBundle\ConnexionBundle(),
             new \FOS\UserBundle\FOSUserBundle(),
+            new Rollerworks\Bundle\PasswordStrengthBundle\RollerworksPasswordStrengthBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
