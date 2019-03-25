cceb0b43c63d15767d71863903aa831651b77765
ih06448 <ih06448@269467bf-4d7a-42d7-80bb-480cca416ccd>
2019-03-25 17:27:50 +0000

    Ajout PublicationType

---
+++ branches/Publish/src/ConnexionBundle/Form/PublicationType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ConnexionBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PublicationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('contenu')->add('Publier',SubmitType::class);
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ConnexionBundle\Entity\Publication'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'connexionbundle_publication';
+    }
+
+
+}
