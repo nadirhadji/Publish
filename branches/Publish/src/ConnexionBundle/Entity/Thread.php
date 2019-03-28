934291f871a26dd043134a2e0cd62fb14daf0183
ii06810 <ii06810@269467bf-4d7a-42d7-80bb-480cca416ccd>
2019-03-28 15:42:10 +0000

    Redirection après le logout vers le login faite et mise en place du bundle pour la messagerie

---
+++ branches/Publish/src/ConnexionBundle/Entity/Thread.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace ConnexionBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use FOS\MessageBundle\Entity\Thread as BaseThread;
+
+/**
+ * Thread
+ *
+ * @ORM\Table(name="thread")
+ * @ORM\Entity(repositoryClass="ConnexionBundle\Repository\ThreadRepository")
+ */
+class Thread extends BaseThread
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="ConnexionBundle\Entity\User")
+     * @var \FOS\MessageBundle\Model\ParticipantInterface
+     */
+    protected $createdBy;
+
+    /**
+     * @ORM\OneToMany(
+     *   targetEntity="ConnexionBundle\Entity\Message",
+     *   mappedBy="thread"
+     * )
+     * @var Message[]|Collection
+     */
+    protected $messages;
+
+    /**
+     * @ORM\OneToMany(
+     *   targetEntity="ConnexionBundle\Entity\ThreadMetadata",
+     *   mappedBy="thread",
+     *   cascade={"all"}
+     * )
+     * @var ThreadMetadata[]|Collection
+     */
+    protected $metadata;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
