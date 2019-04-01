b9442f4a59b051437deac7f18d10d6c13738d0aa
ii11124 <ii11124@269467bf-4d7a-42d7-80bb-480cca416ccd>
2019-04-01 12:01:46 +0000

    Centre d'interet modification

--- branches/Publish/src/ConnexionBundle/Entity/User.php
+++ branches/Publish/src/ConnexionBundle/Entity/User.php
@@ -10,7 +10,6 @@ namespace ConnexionBundle\Entity;
 
 use FOS\UserBundle\Model\User as FosUser;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use FOS\MessageBundle\Model\ParticipantInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 use ConnexionBundle\Entity\Document;
@@ -20,7 +19,7 @@ use ConnexionBundle\Entity\Document;
  * @ORM\Table(name="fos_user")
  */
 
-class User extends FosUser implements ParticipantInterface
+class User extends FosUser
 {
     const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
     const ROLE_ADMIN = 'ROLE_ADMIN';
