1a4111eea01cb7b1cc8f0d829b11ff87307f0e01
ii11124 <ii11124@269467bf-4d7a-42d7-80bb-480cca416ccd>
2019-04-08 13:46:00 +0000

    Centre d'interet fini, les flus rss ont des conditions d'affichage, premiere versio du SearchController

--- src/ConnexionBundle/Controller/RssController.php
+++ src/ConnexionBundle/Controller/RssController.php
@@ -20,35 +20,132 @@ use ConnexionBundle\Form\SearchType;
 class RssController extends Controller
 {
 
+    public function rssLoader(string $url) {
+
+            $RSSItems = array();
+
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $this->getUser();
+
+            $rss = simplexml_load_file($url);
+
+            $images = array();
+            $item = array();
+            $itemObj = array();
+
+            //Insertion de 10 flux RSS liée au centre d'interet
+            for ($i = 0; $i < 10; $i++) {
+
+                //Récupération de l'image ainsi que de l'item a partir du FluxRss (XML)
+
+                $images[$i] = $rss->channel->item[$i]->enclosure['url'];
+                $item[$i] = $rss->channel->item[$i];
+                $itemObj[0] = $images[$i];
+                $itemObj[1] = $item[$i];
+                $RSSItems[$i] = $itemObj;
+
+                //Fin récupération
+
+                //Verification de l'existance de cette item dans la bdd
+                $estDansBase = false;
+
+                //Récupération de la liste des Flux RSS ayant le meme nom que celui qu'on a récupéré a l'itération i de la boucle
+                $ItemBuffer = $em->getRepository('ConnexionBundle:FluxRSS')->findBy(array('nom' => $item[$i]->title));
+
+                //Si la liste retournée par le repository est vide pour l'iteration i, cela veut dire que cette item n'est pas en base donc doit etre ajoutée
+                if($ItemBuffer == null)
+                    $estDansBase = true;
+
+                if($estDansBase) {
+                    //Création de l'objet FluxRSS a inserer en bdd
+
+                    $fluxRss = new FluxRSS();
+                    $fluxRss->setNom($item[$i]->title);
+                    $fluxRss->setUrl($item[$i]->link);
+
+                    //conversion de la date en DateTime
+                    $date = new \DateTime($item[$i]->pubDate);
+                    //fin conversion
+
+                    $fluxRss->setDateFluxRss($date);
+                    $fluxRss->setDescription($item[$i]->description);
+                    $fluxRss->setURLImage($images[$i]);
+                    $fluxRss->setVille("Paris");
+
+                    //Fin de création
+
+                    //Insertion de l'item liée au flux rss en bdd
+
+                    $em = $this->getDoctrine()->getManager();
+
+                    $em->persist($fluxRss);
+
+                    $em->flush();
+
+                    //Fin insertion
+                }
+            }
+
+            return  $RSSItems;
+
+        }
+
     /**
      * @Route("/rss", name="home-rss")
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
      */
-    public function viewRSSAction(Request $request){
+    public function viewRSSAction(Request $request)
+    {
 
-        $rss = simplexml_load_file('https://www.francetvinfo.fr/france.rss');
+        //Récupération des centres d'interet de l'utilisateur connecté
 
-        $images = array();
-        $item = array();
-        $logo = array();
-        $itemObj = array();
         $RSSItems = array();
 
-        for($i = 0 ; $i < 10 ; $i++  ) {
-            $images[$i] = $rss->channel->item[$i]->enclosure['url'];
-            $item[$i] = $rss->channel->item[$i];
-            $itemObj[0] = $images[$i];
-            $itemObj[1] = $item[$i];
-            $RSSItems[$i] = $itemObj;
-            $flusRss = new FluxRSS();
+        $RSSNews = array();
+
+        $RSSSport = array();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $this->getUser();
+
+        $ci = $em->getRepository('ConnexionBundle:CentreInteret')->findBy(array('user' => $user));
+
+
+        //Fin de récupération
+
+
+        // Teste si l'utilisateur s'interesse a l'actualité, si oui on lui retournera les items liées au flux Rss du jourmal LeMonde
+        if ($ci[0]->getIsNews()) {
+
+            $RSSNews = $this->rssLoader('https://www.lemonde.fr/cinema/rss_full.xml');
         }
+
+        if ($ci[0]->getIsSport()) {
+
+            $RSSSport = $this->rssLoader('https://www.lequipe.fr/rss/actu_rss.xml');
+
+        }
+
+        if ($ci[0]->getIsTravel()) {
+
+            $RSSSport = $this->rssLoader('http://o.nouvelobs.com/voyage/rss.xml');
+
+        }
+
+        if ($ci[0]->getIsMusic()) {
+
+            $RSSSport = $this->rssLoader('https://www.nouvelobs.com/mode/rss.xml');
+
+        }
+
+        $RSSItems = array_merge($RSSNews,$RSSSport);
 
         return $this->render('rss.html.twig', array(
             'rssItems' => $RSSItems,
             'logo' => "https://www.lemonde.fr/mmpub/img/lgo/lemondefr_rss.gif"));
-
     }
-
-
 
 }
--- src/ConnexionBundle/Controller/SearchController.php
+++ src/ConnexionBundle/Controller/SearchController.php
@@ -9,27 +9,33 @@
 namespace ConnexionBundle\Controller;
 
 
+use ConnexionBundle\Entity\User;
 use ConnexionBundle\Form\SearchType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Validator\Constraints\Length;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends Controller
 {
     /**
      * @Template
      */
-    public function searchAction(Request $request)
+    public function index(Request $request)
     {
         $search = NULL;
         $formulaire = $this->get('form.factory')->create(SearchType::class);
 
         $formulaire->handleRequest($request);
-        if($formulaire->isSubmitted() && $formulaire->isValid())
-        {
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
             $search = $formulaire['search']->getData();
 
             return $this->redirectToRoute('', array('search' => $search));
@@ -37,5 +43,30 @@ class SearchController extends Controller
         }
         return $this->render('search.html.twig', ['formulaire' => $formulaire->createView()]);
 
+    }
+
+    /**
+     * @Route("/search",
+     *  options = { "expose" = true },
+     *  name = "ajax_search_article",
+     * )
+     */
+    public function searchFriend(Request $request)
+    {
+        $term = $request->request->get('UserVal');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $MatchesUsers = $em->getRepository('ConnexionBundle:User')->findBy(['username' => $term]);
+
+        $data = [];
+
+        foreach ($MatchesUsers as $match) {
+
+            array_push($data,$match->getUsername());
+        }
+
+
+        return  new JsonResponse($data,200);
     }
 }
